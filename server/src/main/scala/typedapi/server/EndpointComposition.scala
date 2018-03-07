8b397f5537b09912a432d722414077744ad449c9
paul-heymann <paul.heymann@xing.com>
2018-03-07 17:00:52 +0100

    added endpoint composition

---
+++ server/src/main/scala/typedapi/server/EndpointComposition.scala
@@ -0,0 +1,32 @@
+package typedapi.server
+
+import shapeless._
+
+sealed trait EndpointComposition
+
+final case class :|:[Fun, T <: EndpointComposition](endpoint: Fun, tail: T) extends EndpointComposition {
+
+  def :|:[FunH](funH: FunH): FunH :|: Fun :|: T = typedapi.server.:|:(funH, this)
+}
+
+case object =: extends EndpointComposition {
+
+  def :|:[Fun](endpoint: Fun) = typedapi.server.:|:(endpoint, this)
+}
+
+sealed trait HListToComposition[H <: HList] {
+
+  type Out <: EndpointComposition
+}
+
+trait HListToCompositionLowPrio {
+
+  implicit val hnilComposition = new HListToComposition[HNil] {
+    type Out = =:.type
+  }
+
+  implicit def consComposition[H <: HList, El <: HList, In <: HList, Out, Fun, T <: HList](implicit next: HListToComposition[T], fun: EndpointFunction.Aux[In, Out, Fun]) = new HListToComposition[(El, In, Out) :: T] {
+
+    type Out = :|:[Fun, next.Out]
+  }
+}
