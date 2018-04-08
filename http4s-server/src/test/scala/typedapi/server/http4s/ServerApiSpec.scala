28463c066db745ed63a5d33537d8f88e9754c31a
pheymann <paulheymann.91@gmail.com>
2018-04-08 22:18:17 +0200

    extended http4s-server specs

--- http4s-server/src/test/scala/typedapi/server/http4s/ServerApiSpec.scala
+++ http4s-server/src/test/scala/typedapi/server/http4s/ServerApiSpec.scala
@@ -1,6 +1,6 @@
 package typedapi.server.http4s
 
-import org.http4s.client.UnexpectedStatus
+import org.http4s.{Headers, Header, Request, Uri}
 import org.http4s.dsl.io._
 import org.http4s.client.blaze._
 import org.http4s.client.dsl.io._
@@ -10,29 +10,52 @@ import org.specs2.mutable.Specification
 
 final class ServerApiSpec extends Specification {
 
+  sequential
+
   import TestServer._
 
   val client = Http1Client[IO]().unsafeRunSync
-  val server = start(BlazeBuilder[IO]).unsafeRunSync()
 
-  "http4s implements TypedApi's server interface" >> {
-    "with request body, without request body and raw response" >> {
-      client.expect[User](PUT(uri("http://localhost:9000/with/reqbody"), User("joe", 27))).unsafeRunSync() === User("joe", 27)
-      client.expect[User]("http://localhost:9000/without/reqbody").unsafeRunSync() === User("joe", 27)
-      client.expect[String]("http://localhost:9000/raw").unsafeRunSync() === "hello"
+  def tests(port: Int) = {
+    "paths and segments" >> {
+        client.expect[User](s"http://localhost:$port/path").unsafeRunSync() === User("joe", 27)
+        client.expect[User](s"http://localhost:$port/segment/jim").unsafeRunSync() === User("jim", 27)
+      }
+  
+      "queries" >> {
+        client.expect[User](s"http://localhost:$port/query?age=42").unsafeRunSync() === User("joe", 42)
+      }
+ 
+      "headers" >> {
+        client.expect[User](Request[IO](method = GET, uri = Uri.fromString(s"http://localhost:$port/header").getOrElse(null), headers = Headers(Header("age", "42")))).unsafeRunSync() === User("joe", 42)
+        client.expect[User](Request[IO](method = GET, uri =Uri.fromString(s"http://localhost:$port/header/raw").getOrElse(null), headers = Headers(Header("age", "42"), Header("name", "jim")))).unsafeRunSync() === User("jim", 42)
+      }
+
+      "methods" >> {
+        client.expect[User](s"http://localhost:$port/").unsafeRunSync() === User("joe", 27)
+        client.expect[User](PUT(Uri.fromString(s"http://localhost:$port/").getOrElse(null))).unsafeRunSync() === User("joe", 27)
+        client.expect[User](PUT(Uri.fromString(s"http://localhost:$port/body").getOrElse(null), User("joe", 27))).unsafeRunSync() === User("joe", 27)
+        client.expect[User](POST(Uri.fromString(s"http://localhost:$port/").getOrElse(null))).unsafeRunSync() === User("joe", 27)
+        client.expect[User](POST(Uri.fromString(s"http://localhost:$port/body").getOrElse(null), User("joe", 27))).unsafeRunSync() === User("joe", 27)
+        client.expect[User](DELETE(Uri.fromString(s"http://localhost:$port/?reasons=because").getOrElse(null))).unsafeRunSync() === User("joe", 27)
+      }
+  }
+
+  val serverDsl = startDsl(BlazeBuilder[IO]).unsafeRunSync()
+  val serverDef = startDef(BlazeBuilder[IO]).unsafeRunSync()
+
+  s"http4s implements TypedApi's server interface" >> {
+    "api dsl" >> {
+      tests(9000)
     }
 
-    "not found" >> {
-      client.expect[String]("http://localhost:9000/wrong").unsafeRunSync() must throwA(UnexpectedStatus(NotFound))
-    }
-
-    "bad request" >> {
-      client.expect[String]("http://localhost:9000/query?query=hello").unsafeRunSync() must throwA(UnexpectedStatus(BadRequest))
-      client.expect[String]("http://localhost:9000/query?wrong=hello").unsafeRunSync() must throwA(UnexpectedStatus(BadRequest))
+    "api definition" >> {
+      tests(10000)
     }
 
     step {
-      server.shutdown.unsafeRunSync()
+      serverDsl.shutdown.unsafeRunSync()
+      serverDef.shutdown.unsafeRunSync()
     }
   }
 }
--- http4s-server/src/test/scala/typedapi/server/http4s/TestServer.scala
+++ http4s-server/src/test/scala/typedapi/server/http4s/TestServer.scala
@@ -1,6 +1,5 @@
 package typedapi.server.http4s
 
-import typedapi.dsl._
 import typedapi.server._
 import org.http4s.server.Server
 import org.http4s.server.blaze.BlazeBuilder
@@ -13,25 +12,68 @@ import cats.effect.IO
 
 object TestServer {
 
-  val Api =
-    (:= :> "without" :> "reqbody" :> Get[User]) :|:
-    (:= :> "with" :> "reqbody" :> ReqBody[User] :> Put[User]) :|:
-    (:= :> "raw" :> Get[String]) :|:
-    (:= :> "query" :> Query[Int]('query) :> Get[String])
+  val ApiDsl = {
+    import typedapi.dsl._
 
-  val withoutReqBody: () => IO[User] = () => IO.pure(User("joe", 27))
-  val withReqBody: User => IO[User] = user => IO.pure(user)
-  val raw: () => IO[String] = () =>  IO.pure("hello")
-  val query: Int => IO[String] = query => IO.pure(query.toString())
+    (:= :> "path" :> Get[User]) :|:
+    (:= :> "segment" :> Segment[String]('name) :> Get[User]) :|:
+    (:= :> "query" :> Query[Int]('age) :> Get[User]) :|:
+    (:= :> "header" :> Header[Int]('age) :> Get[User]) :|:
+    (:= :> "header" :> "raw" :> Header[Int]('age) :> RawHeaders :> Get[User]) :|:
+    (:= :> Get[User]) :|:
+    (:= :> Put[User]) :|:
+    (:= :> "body" :> ReqBody[User] :> Put[User]) :|:
+    (:= :> Post[User]) :|:
+    (:= :> "body" :> ReqBody[User] :> Post[User]) :|:
+    (:= :> Query[List[String]]('reasons) :> Delete[User])
+  }
+
+  val ApiDef = {
+    import typedapi._
+
+    api(Get[User], Root :> "path") :|:
+    api(Get[User], Root :> "segment" :> Segment[String]('name)) :|:
+    api(Get[User], Root :> "query", Queries :> Query[Int]('age)) :|:
+    api(Get[User], Root :> "header", headers = Headers :> Header[Int]('age)) :|:
+    api(Get[User], Root :> "header" :> "raw", headers = Headers :> Header[Int]('age) :> RawHeaders) :|:
+    api(Get[User]) :|:
+    api(Put[User]) :|:
+    apiWithBody(Put[User], ReqBody[User], Root :> "body") :|:
+    api(Post[User]) :|:
+    apiWithBody(Post[User], ReqBody[User], Root :> "body") :|:
+    api(Delete[User], Root, Queries :> Query[List[String]]('reasons))
+  }
+
+  val path: () => IO[User] = () => IO.pure(User("joe", 27))
+  val segment: String => IO[User] = name => IO.pure(User(name, 27))
+  val query: Int => IO[User] = age => IO.pure(User("joe", age))
+  val header: Int => IO[User] = age => IO.pure(User("joe", age))
+  val raw: (Int, Map[String, String]) => IO[User] = (age, nameM) => IO.pure(User(nameM("name"), age))
+  val get: () => IO[User] = () => IO.pure(User("joe", 27))
+  val put: () => IO[User] = () => IO.pure(User("joe", 27))
+  val putB: User => IO[User] = user => IO.pure(user)
+  val post: () => IO[User] = () => IO.pure(User("joe", 27))
+  val postB: User => IO[User] = user => IO.pure(user)
+  val delete: List[String] => IO[User] = reasons => {
+    println(reasons)
+    IO.pure(User("joe", 27))
+  }
 
   implicit val decoder = jsonOf[IO, User]
   implicit val encoder = jsonEncoderOf[IO, User]
 
-  val endpoints = link(Api).to[IO](withoutReqBody :|: withReqBody :|: raw :|: query :|: =:)
+  val endpointsDsl = link(ApiDsl).to[IO](path :|: segment :|: query :|: header :|: raw :|: get :|: put :|: putB :|: post :|: postB :|: delete :|: =:)
+  val endpointsDef = link(ApiDef).to[IO](path :|: segment :|: query :|: header :|: raw :|: get :|: put :|: putB :|: post :|: postB :|: delete :|: =:)
 
-  def start(blaze: BlazeBuilder[IO]): IO[Server[IO]] = {
+  def startDsl(blaze: BlazeBuilder[IO]): IO[Server[IO]] = {
     val sm = ServerManager(blaze, "localhost", 9000)
 
-    mount(sm, endpoints)
+    mount(sm, endpointsDsl)
+  }
+
+  def startDef(blaze: BlazeBuilder[IO]): IO[Server[IO]] = {
+    val sm = ServerManager(blaze, "localhost", 10000)
+
+    mount(sm, endpointsDef)
   }
 }
