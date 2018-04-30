54af485b1f1ace127f61a47e93aaf15569adcd25
paul-heymann <paul.heymann@xing.com>
2018-04-30 20:25:32 +0200

    client: return tuples instead of HList type

---
+++ client/src/main/scala/typedapi/client/ExecutablesFromHList.scala
@@ -0,0 +1,39 @@
+package typedapi.client
+
+import shapeless._
+import shapeless.ops.function.FnFromProduct
+
+/** Derives executables from list of RequestBuilders. */
+sealed trait ExecutablesFromHList[H <: HList] {
+
+  type Out <: HList
+
+  def apply(h: H): Out
+}
+
+object ExecutablesFromHList extends ExecutablesFromHListLowPrio {
+
+  type Aux[H <: HList, Out0 <: HList] = ExecutablesFromHList[H] { type Out = Out0 }
+}
+
+trait ExecutablesFromHListLowPrio {
+
+  implicit val noExecutable = new ExecutablesFromHList[HNil] {
+    type Out = HNil
+
+    def apply(h: HNil): Out = HNil
+  }
+
+  implicit def deriveExcutable[El <: HList, KIn <: HList, VIn <: HList, O, D <: HList, T <: HList](implicit next: ExecutablesFromHList[T],
+                                                                                                            vinToFn: FnFromProduct[VIn => ExecutableDerivation[El, KIn, VIn, O, D]]) = 
+    new ExecutablesFromHList[RequestDataBuilder.Aux[El, KIn, VIn, O, D] :: T] {
+      type Out = vinToFn.Out :: next.Out
+
+      def apply(comps: RequestDataBuilder.Aux[El, KIn, VIn, O, D] :: T): Out = {
+        val fn = vinToFn.apply(input => new ExecutableDerivation[El, KIn, VIn, O, D](comps.head, input))
+
+        fn :: next(comps.tail)
+      }
+    }
+}
+
