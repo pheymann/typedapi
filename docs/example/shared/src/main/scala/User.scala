e1424cfefd6d228df02af691894870a838fde0f3
paul-heymann <paul.heymann@xing.com>
2018-07-03 13:08:09 +0200

    added JS compilation step (broken)

--- docs/example/shared/src/main/scala/User.scala
+++ docs/example/shared/src/main/scala/User.scala
@@ -4,9 +4,6 @@ import io.circe.syntax._
 import io.circe.generic.JsonCodec
 import org.http4s._
 import org.http4s.circe._
-import org.http4s.dsl.io._
-import org.http4s.server.Server
-import org.http4s.server.blaze._
 
 final case class User(name: String, age: Int)
 
